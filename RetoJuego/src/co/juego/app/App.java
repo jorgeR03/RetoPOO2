d6ed9b34cbf0fafc32529ba673379a0217c378f6
Jorge Andres Carmona Ramirez <jorgeandresR03@gmail.com>
2021-10-02 13:08:11 -0500

    RETOPOOJUEGO

--- RetoJuego/src/co/juego/app/App.java
+++ RetoJuego/src/co/juego/app/App.java
@@ -1,7 +1,64 @@
 package co.juego.app;
 
-public class App {
-    public static void main(String[] args) {
+import co.juego.dominio.Juego;
 
+import javax.swing.*;
+import java.util.Arrays;
+
+public class App {
+    public static final ImageIcon icono = new ImageIcon("C:\\RETOPOO2\\rkpp73 (1).jpg");
+    public static final int JUGAR = 0;
+    public static final int VER_PUNTAJES = 1;
+    public static final int INSTRUCCIONES = 2;
+    public static final int CAMBIAR_NOMBRE_JUGADOR = 3;
+    private static Juego play = new Juego();
+
+    public static void main(String[] args) {
+        //Juego play = new Juego();
+        do{
+           int seleccion = JOptionPane.showOptionDialog(null, "Bienvenido al juego.", "Pieda, Papel o Tijera.", 0, JOptionPane.PLAIN_MESSAGE, icono, Arrays.asList("Jugar", "Ver Puntajes", "Instrucciones", "Cambiar Nombre Del Jugador").toArray(), play.getNombreJugador());
+           switch (seleccion){
+               case JUGAR: {
+                    if(play.getNombreJugador() == null) {
+                        play.setNombreJugador(ingresarNombre());
+                    }
+                    int jugadaJugador = JOptionPane.showOptionDialog(null, "Selecciona tu jugada.","Piedra, Papel o Tijera",0, JOptionPane.QUESTION_MESSAGE, null, Arrays.asList("Piedra", "Papel", "Tijera").toArray(), null);
+                    if (jugadaJugador == JOptionPane.CLOSED_OPTION){
+                        continue;
+                    }
+                    String resultado = play.jugar(jugadaJugador);
+                    JOptionPane.showMessageDialog( null, "La jugada de la maquina fue: " + play.getJugadaMaquina() + "\nLa jugada de " + play.getNombreJugador() + " fue: " + jugadaJugador + "\n" + resultado);
+                    break;
+               }
+               case VER_PUNTAJES: {
+                   String resultados = "Ganadas Maquina: " + play.getGanadasMaquina() + "\nGanadas " + play.getNombreJugador() + ": " + play.getGanadasJugador() + "\nEmpates: " + play.getEmpates();
+                   JOptionPane.showMessageDialog(null, resultados);
+                   break;
+               }
+               case INSTRUCCIONES:{
+                   String instrucciones = "Instrucciones.\nBienvenido a piedra, papel o tijera:\n- Presionar “Jugar” para iniciar el juego. Si no has ingresado un nombre aún, él te lo pedirá\n  automáticamente. Posteriormente deberás escoger tu jugada, recuerda que la piedra le\n  gana a la tijera, la tijera le gana al papel y el papel le gana a la piedra.\n- Presiona “Ver resultados” para ver el historial de partidas jugadas.\n- Presiona “Cambiar nombre” para cambiar el nombre del jugador. Al realizar esta acción\n  se borrará el historial de partidas.";
+                   JOptionPane.showMessageDialog(null, instrucciones);
+                   break;
+               }
+               case CAMBIAR_NOMBRE_JUGADOR: {
+                   play.setNombreJugador(ingresarNombre());
+                   play.setEmpates(0);
+                   play.setGanadasJugador(0);
+                   play.setGanadasMaquina(0);
+                   break;
+               }
+               case JOptionPane.CLOSED_OPTION:{
+                   System.exit(0);
+               }
+           }
+        }while(true);
     }
+    public static String ingresarNombre(){
+        String nombre;
+        do{
+            nombre = JOptionPane.showInputDialog("Ingrese su nombre.", play.getNombreJugador());
+        }while(nombre == null || nombre.trim().isEmpty());
+        return nombre;
+    }
+
 }
--- RetoJuego/src/co/juego/dominio/Juego.java
+++ RetoJuego/src/co/juego/dominio/Juego.java
@@ -1,50 +1,54 @@
 package co.juego.dominio;
 
+import java.util.Random;
+
 public class Juego {
-    private String nombreJugador;
+    private String nombreJugador = null;
     public static final int PIEDRA = 0;
     public static final int PAPEL = 1;
     public static final int TIJERA = 2;
-    private int jugadaJugador;
-    private int jugadaMaquina;
     private int ganadasJugador = 0;
     private int ganadasMaquina = 0;
-    private int empate = 0;
+    private int empates = 0;
+    private int jugadaMaquina = new Random().nextInt(3);
 
-    public Juego(int jugadaJugador) {
-        this.jugadaJugador = jugadaJugador;
-        this.jugadaMaquina =(int) Math.random()*3;
-    }
-
-    public String jugar(int jugadaJugador, int jugadaMaquina){
-        if(jugadaJugador == PIEDRA && jugadaMaquina == TIJERA){
+    public String jugar(int jugadaJugador) {
+        if (jugadaJugador == PIEDRA && jugadaMaquina == TIJERA) {
             ganadasJugador++;
             return "¡Felicidades, ganaste " + nombreJugador + "!";
-        }
-        else if(jugadaJugador == PAPEL && jugadaMaquina == PIEDRA){
+        } else if (jugadaJugador == PAPEL && jugadaMaquina == PIEDRA) {
             ganadasJugador++;
             return "¡Felicidades, ganaste " + nombreJugador + "!";
-        }
-        else if(jugadaJugador == TIJERA && jugadaMaquina ==PAPEL){
+        } else if (jugadaJugador == TIJERA && jugadaMaquina == PAPEL) {
             ganadasJugador++;
             return "¡Felicidades, ganaste " + nombreJugador + "!";
-        }
-        else if(jugadaJugador == jugadaMaquina){
-            empate++;
-            return "Buen juego" + nombreJugador + ", fue un empate.";
-        }
-        else{
+        } else if (jugadaJugador == jugadaMaquina) {
+            empates++;
+            return "Buen juego " + nombreJugador + ", fue un empate.";
+        } else {
             ganadasMaquina++;
-            return "Lo siento" + nombreJugador + ", perdiste.";
+            return "Lo siento " + nombreJugador + ", perdiste.";
         }
-    }
-
-    public String getNombreJugador() {
-        return nombreJugador;
     }
 
     public void setNombreJugador(String nombreJugador) {
         this.nombreJugador = nombreJugador;
+    }
+
+    public void setGanadasJugador(int ganadasJugador) {
+        this.ganadasJugador = ganadasJugador;
+    }
+
+    public void setGanadasMaquina(int ganadasMaquina) {
+        this.ganadasMaquina = ganadasMaquina;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+
+    public String getNombreJugador() {
+        return nombreJugador;
     }
 
     public int getGanadasJugador() {
@@ -55,7 +59,11 @@ public class Juego {
         return ganadasMaquina;
     }
 
-    public int getEmpate() {
-        return empate;
+    public int getEmpates() {
+        return empates;
+    }
+
+    public int getJugadaMaquina() {
+        return jugadaMaquina;
     }
 }
